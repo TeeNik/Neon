69be3bd7c9a9d4a86b10f8df98fa7210a8bb0d8d
TeeNik <teenik3315@gmail.com>
2018-07-04 22:52:45 +0300

    init

---
+++ Source/Neon/Neon.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Neon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Neon, "Neon" );
+
+DEFINE_LOG_CATEGORY(LogNeon)
+ 
