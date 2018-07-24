52ca581ae99e80c17976a2a8c4189823e4eaecfb
TeeNik <teenik3315@gmail.com>
2018-07-25 01:45:45 +0300

    action table data [2]

--- Source/Neon/Public/Action/ActionTableData.h
+++ Source/Neon/Public/Action/ActionTableData.h
@@ -7,8 +7,18 @@ USTRUCT(BlueprintType)
 struct NEON_API FActionTableData : public FTableRowBase
 {
 	GENERATED_BODY()
+
+public:
 	
-	
-	
-	
+	FActionTableData() {
+		Cost = 2;
+		Description = TEXT("None");
+	};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action Data")
+	FString Description;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action Data")
+	int32 Cost;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action Data")
+	UTexture* Icon;
 };
