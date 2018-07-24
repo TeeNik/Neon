f4f66c118316e2992c3e74bbc76505f1831b8613
TeeNik <teenik3315@gmail.com>
2018-07-25 01:45:01 +0300

    action table

---
+++ Source/Neon/Public/Action/ActionTableData.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "ActionTableData.generated.h"
+
+USTRUCT(BlueprintType)
+struct NEON_API FActionTableData : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
