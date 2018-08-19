af99a38848ec71eba517ab574b8fd8d04ec588c2
TeeNik <teenik3315@gmail.com>
2018-08-19 22:01:17 +0300

    remove  components from character

--- Source/Neon/NeonCharacter.h
+++ Source/Neon/NeonCharacter.h
@@ -14,23 +14,11 @@ class ANeonCharacter : public ACharacter
 
 public:
 	ANeonCharacter();
-
-	/*FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	FORCEINLINE class UDecalComponent* GetCursorToWorld() { return CursorToWorld; }*/
 	FORCEINLINE class UAbilityComponent* GetAbilityConponent() { return AbilityComp; }
 
 	void BeginPlay() override;
 
 private:
-	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* TopDownCameraComponent;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UDecalComponent* CursorToWorld;*/
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "NeonComponent", meta = (AllowPrivateAccess = "true"))
 	class UAbilityComponent* AbilityComp;
