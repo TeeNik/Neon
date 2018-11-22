3172b56fc7476c05c6f28ec02181b1a11004886f
TeeNik <teenik3315@gmail.com>
2018-11-22 22:05:08 +0300

    shoot state

--- Source/Neon/Private/AI/ShootState.cpp
+++ Source/Neon/Private/AI/ShootState.cpp
@@ -2,11 +2,15 @@
 #include "ShootState.h"
 #include "AI/AIStateMachine.h"
 #include "Components/WeaponComponent.h"
+#include "Components/MotionComponent.h"
 #include "Components/AbilityComponent.h"
+#include "GameFramework/Actor.h"
 
-ShootState::ShootState(UAIStateMachine* ai, FName& targetTag, UWeaponComponent* weaponComponent, UAbilityComponent* abilityComp) : AIState(ai)
+ShootState::ShootState(UAIStateMachine* ai, FName& targetTag, UWeaponComponent* weaponComp, UAbilityComponent* abilityComp) : AIState(ai)
 {
-
+	TargetTag = targetTag;
+	WeaponComp = weaponComp;
+	AbilityComp = abilityComp;
 }
 
 ShootState::~ShootState()
@@ -15,5 +19,15 @@ ShootState::~ShootState()
 
 void ShootState::Execute()
 {
-
+	FString name = TEXT("Shoot");
+	TArray<FHitResult> actors = AbilityComp->GetActorsInRange(name);
+	for (auto It = actors.CreateIterator(); It; It++)
+	{
+		auto actor = It->Actor;
+		if (actor->ActorHasTag(TargetTag)) {
+			UMotionComponent* motionComp = Cast<UMotionComponent>(actor->GetComponentByClass(UMotionComponent::StaticClass()));
+			WeaponComp->Shoot(motionComp);
+			break;
+		}
+	}
 }
