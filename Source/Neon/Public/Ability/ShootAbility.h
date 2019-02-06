3ff23dc0e188736ba458e51fda9f2191ab736754
TeeNik <teenik3315@gmail.com>
2019-02-06 23:56:58 +0300

    shoot ability

---
+++ Source/Neon/Public/Ability/ShootAbility.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ability/Ability.h"
+#include "ShootAbility.generated.h"
+
+UCLASS()
+class NEON_API UShootAbility : public UAbility
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
