2fe2de64d6aa40d3890c28d225967f94ba4a4c21
TeeNik <teenik3315@gmail.com>
2019-03-27 07:58:50 +0300

    range

--- Source/Neon/Private/WorldActors/DBQuestActor.cpp
+++ Source/Neon/Private/WorldActors/DBQuestActor.cpp
@@ -1,5 +1,7 @@
 #include "DBQuestActor.h"
 #include "Components/BoxComponent.h"
+#include "NeonPlayerController.h"
+#include "NeonCharacter.h"
 #include "Components/StaticMeshComponent.h"
 #include "Materials/MaterialInstance.h"
 
@@ -30,6 +32,13 @@ void ADBQuestActor::OnEndCursorOver(UPrimitiveComponent* TouchedComponent)
 
 void ADBQuestActor::OnClicked(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed)
 {
+    ANeonPlayerController* PC = Cast<ANeonPlayerController>(AI->GetWorld()->GetFirstPlayerController());
+    ANeonCharacter* player = PC->NeonCharacter;
+    float range = 350;
+    float dist = FVector::Dist(player->GetActorLocation(), AI->GetOwner()->GetActorLocation());
+    if (dist <= range)
+    {
 
+    }
 }
 
