a67ebb5860596784d52b9a9e75042b6c0a7990a8
TeeNik <teenik3315@gmail.com>
2019-03-29 09:22:27 +0300

    dbquest [4]

--- Source/Neon/Private/WorldActors/DBQuestActor.cpp
+++ Source/Neon/Private/WorldActors/DBQuestActor.cpp
@@ -1,6 +1,6 @@
 #include "DBQuestActor.h"
 #include "Components/BoxComponent.h"
-#include "NeonPlayerController.h"
+#include "NeonGameMode.h"
 #include "NeonCharacter.h"
 #include "Components/StaticMeshComponent.h"
 #include "Materials/MaterialInstance.h"
@@ -22,16 +22,36 @@ void ADBQuestActor::BeginPlay()
 
 void ADBQuestActor::OnBeginCursorOver(UPrimitiveComponent* TouchedComponent)
 {
-    SelectionCircle->SetMaterial(0, HighlightMaterial);
+    if (isInRange) {
+        SelectionCircle->SetMaterial(0, HighlightMaterial);
+    }
 }
 
 void ADBQuestActor::OnEndCursorOver(UPrimitiveComponent* TouchedComponent)
 {
+    if (isInRange) {
+        SelectionCircle->SetMaterial(0, DefaultMaterial);
+    }
+}
+
+void ADBQuestActor::Deactivate()
+{
+    isInRange = false;
     SelectionCircle->SetMaterial(0, DefaultMaterial);
+}
+
+bool ADBQuestActor::Highlight(FString& AbilityName)
+{
+    isInRange = true;
+    return true;
 }
 
 void ADBQuestActor::OnClicked(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed)
 {
+    if (isInRange) {
+        ANeonGameMode* GM = Cast<ANeonGameMode>(GetWorld()->GetAuthGameMode());
+
+    }
     ANeonPlayerController* PC = Cast<ANeonPlayerController>(AI->GetWorld()->GetFirstPlayerController());
     ANeonCharacter* player = PC->NeonCharacter;
     float range = 350;
--- Source/Neon/Public/WorldActors/DBQuestActor.h
+++ Source/Neon/Public/WorldActors/DBQuestActor.h
@@ -1,23 +1,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "GameFramework/Actor.h"]
+#include "Action/Action.h"
 #include "DBQuestActor.generated.h"
 
 class UMaterialInstance;
 class UBoxComponent;
 
 UCLASS()
-class NEON_API ADBQuestActor : public AActor
+class NEON_API ADBQuestActor : public AActor, public IAction
 {
 	GENERATED_BODY()
 	
 public:	
 	ADBQuestActor();
 
-    void OnBeginCursorOver(UPrimitiveComponent* TouchedComponent);
-    void OnEndCursorOver(UPrimitiveComponent* TouchedComponent);
-    void OnClicked(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed);
+    UFUNCTION(BlueprintCallable, Category = "Action")
+    virtual void OnBeginCursorOver(UPrimitiveComponent* TouchedComponent) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Action")
+    virtual void OnEndCursorOver(UPrimitiveComponent* TouchedComponent) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Action")
+    virtual void OnClicked(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Action")
+    virtual void Deactivate() override;
+
+    UFUNCTION(BlueprintCallable, Category = "Action")
+    virtual bool Highlight(FString& AbilityName) override;
 
 protected:
 	virtual void BeginPlay() override;
