25ac2fa9134666909c332150567fb6cd307b24f3
TeeNik <teenik3315@gmail.com>
2019-06-11 23:10:25 +0300

    start crouch

--- Source/Neon/Private/Character/NeonAIController.cpp
+++ Source/Neon/Private/Character/NeonAIController.cpp
@@ -2,6 +2,7 @@
 #include "System/UtilsLibrary.h"
 #include "AI/AIStateMachine.h"
 #include "AI/IdleState.h"
+#include "GameFramework/Character.h"
 
 void ANeonAIController::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result) {
     Super::OnMoveCompleted(RequestID, Result);
@@ -9,7 +10,10 @@ void ANeonAIController::OnMoveCompleted(FAIRequestID RequestID, const FPathFollo
     GLog->Log("MoveCompleted");
 
 	APawn* pawn = GetPawn();
-	if (pawn->ActorHasTag("Enemy")) {
+    AActor* actor = Cast<AActor>(pawn);
+    ACharacter* character = Cast<ACharacter>(actor);
+    character->Crouch();
+	if (character->ActorHasTag("Enemy")) {
 		UAIStateMachine* AI = UUtilsLibrary::GetComponentByClass<UAIStateMachine>(pawn);
 		UAIState* state = NewObject<UIdleState>(this, UIdleState::StaticClass());
 		AI->NextState(state);
